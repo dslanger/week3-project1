25be621d6e6158ed1466f8ba60470a5658b65a32
dslanger <dslanger@gmail.com>
2016-02-03 23:13:04 -0700

    11-17 w/ erros

--- main.js
+++ main.js
@@ -118,10 +118,19 @@ backwards();
  var moneyInTheBank = function (quarters, dimes, nickels, pennies) {
  	var value = quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + pennies * 0.01;
  	value.toFixed(2);
- 	console.log("$" + value);
+ 	//console.log("$" + value);
  };
  moneyInTheBank(3, 1, 2, 5);
 // 11. Develop a function that determines a person's age by prompting them for their birth year.
+var ager = function (birthYear) {
+	//var time = new Date();
+	//var currentYear = time.getFullYear;
+	var currentYear = 2016;
+	age = currentYear - birthYear;
+	console.log(age);
+
+};
+ager(1991);
 
 // 12. Write a function that takes a year and reports whether or not it is a leap year.
 //     Remember, a leap year occurs:
@@ -130,6 +139,16 @@ backwards();
 //        Unless the year is also evenly divisible by 400
 //        For example, 1997 is not a leap year, but 1996 is.  1900 is not a leapyear, but 2000 is
 
+var leapYear = function (year) {
+	if ( year % 4 === 0 && year % 400 !== 0 && year % 100 !== 0) {
+		console.log(year + " is a leap year");
+	} else {
+		console.log(year + " is not a leap year");
+	}
+};
+leapYear(1997);
+leapYear(1996);
+
 // 13. Develop a function that cleans up a phone number entered by a user.
 //     The rules are as follows:
 //        If the phone number is less than 10 digits assume that it is bad number
@@ -137,17 +156,143 @@ backwards();
 //        If the phone number is 11 digits and the first number is 1, trim the 1 and use the last 10 digits
 //        If the phone number is 11 digits and the first number is not 1, then it is a bad number
 //        If the phone number is more than 11 digits assume that it is a bad number
+var phoneFresh = function (number) {
+	var number = number.toString();
+	if (number.length < 10) {
+		console.log("Hm, you're number has fewer than 10 digits. Please enter a number with at least 10 digits.");
+	} else if (number.length == 10) {
+		console.log("I'm assuming this is a good number.");
+	} else if (number.length == 11 && number.charAt(0) !== 1) {
+		console.log("I'm assuming this is a bad number.");
+	} else if (number.length > 11) {
+		console.log("I'm assuming this is a bad number.");
+	} else {
+		console.log("Hm, I'm not sure what to make of this number. Can you confirm it's the right number?");
+	}
+};
+phoneFresh(3032938544);
 
 // 14. Create a function that determines whether a parameter is a number or not (Hint: google "isNan")
 //     Iterate over the elements in the following array to determine if each is a number.
        arrayOfAllTheThings = ["one", 23, {thingsWhalesLove: "beaches"}, "six hundred", 33, 6834, "5,435"]
 
-// 15. Create a die rolling function that accepts two parameters (the two six-sided dice) and outputs an array of the two values rolled.
+var numberCheck = arrayOfAllTheThings.map(function (toCheck) {
+	if (isNaN(toCheck) === true) {
+		console.log(toCheck + " is not a number");
+	} else {
+		console.log(toCheck + " is a number");
+	}
+});
+
+
+//15. Create a die rolling function that accepts two parameters (the two six-sided dice) and outputs an array of the two values rolled.
+var dieRoll1 = function (roll1, roll2) {
+	if (roll1 !== 6) { 
+		roll1 = 6;
+	}
+	if (roll2 !== 6) { 
+		roll2 = 6;
+	} 
+	var break1 = Math.floor(Math.random() * roll1) + 1;
+	var break2 = Math.floor(Math.random() * roll2) + 1;
+	var theBreaks = [break1, break2];
+	
+	console.log(theBreaks);
+	return theBreaks;
+}
+dieRoll1(1, 1);
+
+var dieRoll2 = function () {
+	var theBreaks = [];
+	theBreaks.push(Math.floor(Math.random() * 6) + 1);
+	theBreaks.push(Math.floor(Math.random() * 6) + 1);  
+	return theBreaks;
+
+}
+dieRoll2();
+
+var dieRoll3 = function (numberOfDice) {
+	var theBreaks = [];
+	for (var i = 0; i < numberOfDice; i++)
+	theBreaks.push(Math.floor(Math.random() * 6) + 1);
+	return theBreaks;
+
+}
+dieRoll3(2);
 
 // 16. Create another fuction called "whichSide". It will accept the array from above as it's only parameter.
 //     This function will mimic a Monopoly board. Start playerOne at the Go space (0) and add the two values from your die array to determine
-//     how many spaces the player will move. Log to the console which side of the board the player is on.
-//     Hint: each side has 11 spaces. You can call the sides of the board: north, east, south, west or top, right, bottom, left.
+//     how many square the player will move. Log to the console which side of the board the player is on.
+//     Hint: each side has 11 square. You can call the sides of the board: north, east, south, west or top, right, bottom, left.
+var square = 0;
+var whichSide = function (param) {
+	var roll = param;
+	for (var i = 0; i < roll.length; i++) {
+		square += roll[i];
+	}
+	if (square <= 10) {
+		console.log("Player moves to " + square + " and is on the south side of the neighboard.");
+	}
+	if (square <= 20 && square >= 10) {
+		console.log("Player moves to " + square + " and is on the west side of the neighboard.");
+	}
+	if (square <= 30 && square >= 20) {
+		console.log("Player moves to " + square + " and is on the north side of the neighboard.");
+	}
+	if (square <= 40 && square >= 30) {
+		console.log("Player moves to " + square + " and is on the east side of the neighboard.");
+	}
+}
+
+whichSide(dieRoll2());
+
 
 // 17. Using the game above, use the same function(s) and add additional code to determine if the player is on a corner spot.
 //     Log to the console which corner the player is located.
+var dieRoll2 = function () {
+	var theBreaks = [];
+	theBreaks.push(Math.floor(Math.random() * 6) + 1);
+	theBreaks.push(Math.floor(Math.random() * 6) + 1);  
+	return theBreaks;
+};
+//dieRoll2();
+
+var square = 0;
+var whichSide = function (param) {
+	var roll = param;
+	var rollValue = 0;
+	for (var i = 0; i < roll.length; i++) {
+		rollValue += roll[i];
+		square += roll[i];
+	}
+	console.log("Player rolls a " + rollValue);
+	if (square > 40) {
+		square = square % 40;
+	}
+	if (square < 10) {
+		console.log("Player moves to square " + square + " and is on the south side of the neighboard.");
+	}
+	if (square == 10) {
+		console.log("Player moves to square " + square + " and is on the jail corner");
+	} 
+	if (square < 20 && square > 10) {
+		console.log("Player moves to square " + square + " and is on the west side of the neighboard.");
+	}
+	if (square == 20) {
+		console.log("Player moves to square " + square + " and is on the free parking corner");
+	} 
+	if (square < 30 && square > 20) {
+		console.log("Player moves to square " + square + " and is on the north side of the neighboard.");
+	}
+	if (square == 30) {
+		console.log("Player moves to square " + square + " and is on the go to jail corner");
+	} 
+	if (square < 40 && square > 30) {
+		console.log("Player moves to square " + square + " and is on the east side of the neighboard.");
+	}
+	if (square == 40) {
+		console.log("Player moves to square " + square + " and is on the go corner");
+	} 
+};
+whichSide(dieRoll2());
+
